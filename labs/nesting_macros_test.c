faf9489494246f371813ff918770005e2aed5c8b
Roger Kowalewski <rkowalewski@rkowalewski.local>
2014-06-17 17:13:21 +0200

    started to check how we can improve handling of nested macros

--- labs/nesting_macros_test.c
+++ labs/nesting_macros_test.c
@@ -6,11 +6,31 @@
 #define __ONE_OR_TWO_ARGS(NAME, N, ...) _ONE_OR_TWO_ARGS_ ## N (NAME, __VA_ARGS__)
 #define _ONE_OR_TWO_ARGS(NAME, N, ...) __ONE_OR_TWO_ARGS(NAME, N, __VA_ARGS__)
 #define ONE_OR_TWO_ARGS(NAME, ...) NAME(_ONE_OR_TWO_ARGS(NAME, NARG2(__VA_ARGS__), __VA_ARGS__))
-#define one_or_two(...) ONE_OR_TWO_ARGS(one_or_two, __VA_ARGS__)
+#define one_or_two(...) ONE_OR_TWO_ARGS(one_or_two, __VA_ARGS__) 
 
-void one_or_two(int a, int b) { printf("%s seeing a=%d and b=%d\n", __func__, a, b); }
-static inline int one_or_two_default_arg_1(void) {  return 5; }
- 
+#define EXPECT_EQ_INT(actual, expected) \
+  EXPECT_EQ_INTERN(INT, ONE_OR_TWO_ARGS(actual), ONE_OR_TWO_ARGS(expected))
+
+#define EXPECT_EQ_INTERN(INT, actual, expected)
+
+enum return_type {
+  INT
+};
+
+typedef struct Unknown_Type {
+  enum return_type returnType;
+  int isNested;
+  int value;
+  int rank;
+} Unknown_Type;
+
+void one_or_two(int a, int b) {
+  printf("%s seeing a=%d and b=%d\n", __func__, a, b);
+}
+static inline int one_or_two_default_arg_1(void) {
+  return 5;
+}
+
 int main (void) {
   one_or_two(6);
   one_or_two(6,10);
